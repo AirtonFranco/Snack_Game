47d9e8b601203519fee3fe1d5a47928a5a82edc0
AirtonSilveira <a.silveira@accesscredito.com.br>
2020-05-14 13:43:24 -0400

    definindo os movimentos da snake

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right"
 
 function criarBG() {
     context.fillStyle = "lightgreen" ;//O fillStyle trabalha com o estilo do nosso canvas
@@ -19,5 +20,29 @@ function criarCobrinha() {
     }
 }
 
-criarBG();
-criarCobrinha();
+//Vamos criar uma funcao para atualizar o jogo
+function iniciarJogo() {
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+    
+    //Cordenadas
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    //Adicionando um elemento na snake
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100);
